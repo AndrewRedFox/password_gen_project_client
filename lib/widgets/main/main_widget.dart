0290db62f0278b92b9125ea640ddbeb49ed06af9
AndrewKushnir <cuckop30@gmail.com>
2024-01-23 22:02:51 +0300

    add support different dispay mode

--- lib/widgets/main/main_widget.dart
+++ lib/widgets/main/main_widget.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_displaymode/flutter_displaymode.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:password_gen_project/client/client_api_user_get_info.dart';
 import 'package:password_gen_project/theme/app_colors.dart';
@@ -15,6 +17,7 @@ class _MainWidgetState extends State<MainWidget> {
   @override
   void initState() {
     WidgetsBinding.instance.addPostFrameCallback((_) { auth();});
+    setOptimalDisplayMode();
     super.initState();
   }
 
@@ -23,6 +26,28 @@ class _MainWidgetState extends State<MainWidget> {
       Navigator.of(context).pushReplacementNamed("/listOfPassword");
     } else{
       Navigator.of(context).pushReplacementNamed("/auth");
+    }
+  }
+
+  Future<void> setOptimalDisplayMode() async {
+    try {
+      final List<DisplayMode> supported = await FlutterDisplayMode.supported;
+
+      final DisplayMode active = await FlutterDisplayMode.active;
+
+    final List<DisplayMode> sameResolution = supported.where(
+            (DisplayMode m) => m.width == active.width
+                && m.height == active.height).toList()..sort(
+            (DisplayMode a, DisplayMode b) =>
+                b.refreshRate.compareTo(a.refreshRate));
+
+    final DisplayMode mostOptimalMode = sameResolution.isNotEmpty
+        ? sameResolution.first
+        : active;
+
+    await FlutterDisplayMode.setPreferredMode(mostOptimalMode);
+    } on PlatformException catch (e) {
+      developer.log("${e}");
     }
   }
 
